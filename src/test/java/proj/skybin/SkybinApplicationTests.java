0f0ed481af4b7be0520dff299dd299433b795459
mliamsinclair <m.liam.sinclair@gmail.com>
2023-10-23 22:40:21 -0300

    added dependency to pom.xml added test to SkybinApplicationTests.java

--- src/test/java/proj/skybin/SkybinApplicationTests.java
+++ src/test/java/proj/skybin/SkybinApplicationTests.java
@@ -3,11 +3,14 @@ package proj.skybin;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 @SpringBootTest
 class SkybinApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertNotNull("The context should have loaded successfully");
 	}
 
 }
