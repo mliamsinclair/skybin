d02cf212ba69be8bfa0f4edae40ddbedfb99fdbd
mliamsinclair <m.liam.sinclair@gmail.com>
2023-10-24 17:01:26 -0400

    macos work

---
+++ src/main/java/proj/skybin/model/Role.java
@@ -0,0 +1,27 @@
+package proj.skybin.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
+    private int id;
+    @Column(name = "role")
+    private String role;
+}
--- src/main/java/proj/skybin/repository/UserRepository.java
+++ src/main/java/proj/skybin/repository/UserRepository.java
@@ -1,14 +1,11 @@
 package proj.skybin.repository;
 
+import proj.skybin.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import proj.skybin.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    public User findByUsername(String username);
-
-    public User findByEmail(String email);
-
-    public User findByUsernameAndPassword(String username, String password);
+    User findByEmail(String email);
+    User findByUserName(String userName);
 }
